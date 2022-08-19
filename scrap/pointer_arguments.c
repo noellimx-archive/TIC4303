56b928522da10b9833ee31b6fb0d10f7f3e1782f
Noel Lim <noellimx@gmail.com>
2022-08-20 03:45:31 +0800

    ---

--- scrap/pointer_arguments.c
+++ scrap/pointer_arguments.c
@@ -2,6 +2,18 @@
 
 #include <stdio.h>
 
+void some(char **ptr_chr_ptr)
+{
+    printf("start of some\n");
+    printf("\n");
+    printf(ptr_chr_ptr);
+    printf("\n dereference \n");
+    printf("%d\n", *ptr_chr_ptr);
+    printf("\n dereference dereference\n");
+    printf("%d\n", **ptr_chr_ptr);
+    printf("\nend of some");
+}
+
 int main(void)
 {
     char chr = 'a';
@@ -10,13 +22,15 @@ int main(void)
     (*chr_ptr); // ok
 
     chr_ptr = &chr;
-
+    printf("--- %d\n", chr_ptr);
     // not ok (*(*chr_ptr));
 
     (&chr_ptr);
 
     (*(&chr_ptr));
 
-    printf("%c", *(*(&chr_ptr)));
+    printf("%c\n", *(*(&chr_ptr)));
+
+    some(&chr_ptr);
     return -1;
 }
