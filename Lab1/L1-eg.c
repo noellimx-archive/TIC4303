c671cc5e8dc0f924989e09ea383dd297d3fb643e
Noel Lim <noellimx@gmail.com>
2022-08-22 18:59:58 +0800

    reorg

--- Lab1/L1-eg.c
+++ Lab1/L1-eg.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void do_reverse(void) {
+void do_reverse(void)
+{
 	int l, i, c;
 	char *p;
 
-	p = (char *) malloc(256);
-	for (i=0; (c = getchar()) != '\n'; i++) 
+	p = (char *)malloc(256);
+	for (i = 0; (c = getchar()) != '\n'; i++)
 		p[i] = c;
 	l = i;
 	for (; i > 0; i--)
-		putchar(p[i-1]);
-	if (l > 0) putchar('\n');
+		putchar(p[i - 1]);
+	if (l > 0)
+		putchar('\n');
 }
-
