8e9929b378c4f02db661d35360a245058bc81d2c
Noel Lim <noellimx@gmail.com>
2022-08-30 23:20:41 +0800

    -

--- Lab2/L2-mem-region-change-sizeofroster.c
+++ Lab2/L2-mem-region-change-sizeofroster.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[], char* envp[]){
 	char buf[256], local[16];
 	void *heap;
 
-	printf("%d",sizeof(char *));
+	printf("%lu",sizeof(char *));
 	addr(&buf, "addr of ptr to buf");
 	addr(&local, "addr of ptr to local");
 	addr(heap, "addr of heap");
