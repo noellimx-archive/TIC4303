424a05e069a16f4be1671abc231c8c1dbfdc8c92
Noel Lim <noellimx@gmail.com>
2022-08-31 16:06:12 +0800

    -

--- Lab2/rr.c
+++ Lab2/rr.c
@@ -40,7 +40,9 @@ int main(int argc, char *argv[], char *envp[])
 	case 1:
 		p = 0;
 		printf("in case 1, p reassigned\n");
-		setvalue(&p, 0);
+
+		printf("WHAT IS NULL %d\n", NULL);
+		setvalue(p, 0);
 		break;
 	case 2:
 		p = hello;
