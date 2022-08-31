7a203b52a8a01348bbfd014ea8dfc5f9432370d9
Noel Lim <noellimx@gmail.com>
2022-08-31 13:27:05 +0800

    experiment

--- Lab2/L2-mem-region.c
+++ Lab2/L2-mem-region.c
@@ -20,10 +20,19 @@ void addr(void *ptr, char *s)
 void f(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6,
 	   int arg7, int arg8)
 {
+
+	char *cPtr_before_local = (char *)malloc(sizeof(char) * 1);
 	char local[32];
-	addr(&arg1, "f(arg1)");
-	addr(&arg6, "f(arg6)");
+	char *cPtr_after_local = (char *)malloc(sizeof(char) * 1);
+
 	addr(local, "f():local");
+
+	addr(&arg1, "f(arg1)");
+	addr(&arg2, "f(arg2)");
+	addr(&arg3, "f(arg3)");
+	addr(&arg4, "f(arg4)");
+	addr(&arg5, "f(arg5)");
+	addr(&arg6, "f(arg6)");
 }
 
 int main(int argc, char *argv[], char *envp[])
@@ -45,7 +54,7 @@ int main(int argc, char *argv[], char *envp[])
 	f(1, 2, 3, 4, 5, 6, 7, 8);
 	addr(argv[0], "argv[0], which is a char array"); // argv[0] has type (char *)
 	addr(envp[0], "envp[0], which is a char array");
-	addr(&errno, "errno"); // comes from errno.h
+	addr(&errno, "errno");	// comes from errno.h
 	addr(stdout, "stdout"); // comes from stdio.h
 	fflush(stdout);
 
--- Lab2/submission_lab2_exercise_1.md
+++ Lab2/submission_lab2_exercise_1.md
@@ -7,4 +7,10 @@
 
 A hexidecimal digit represents 4 binary digits. i.e 0x7ffef87cc6fc == 0b011111111111111011111000011111001100011011111100. It is much more human-readable to display in hex. Also, address bus sizes are usually in shifts of 8 bits which can be represented by 2 hexes.
 
-##
+## What is “rostr”, and the difference with “&rostr”?
+
+```rostr``` is the pointer to the char array. Also the address stores the first element in the array.
+
+```&rostr``` is of type **char; the address which stores the pointer to the char array.
+
+## Which direction does the runtime stack grow?
