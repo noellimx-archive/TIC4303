7d0d38777b2dad9ed0e08705e4938308fa6de0c1
Noel Lim <noellimx@gmail.com>
2022-10-15 17:55:35 +0800

    -

--- Lab6/L6-function-ptr-q2.c
+++ Lab6/L6-function-ptr-q2.c
@@ -31,6 +31,7 @@ int main(int argc, char *argv[], char *envp[])
 	f = addnum;
 
 	printf("LINE1 f: %p\n", f);
+	printf("is addnum == f : %i", f == addnum);
 	a = f(10); // LINE1
 	printf("1: f(10)=%d f=%p\n\n", a, f);
 
@@ -41,6 +42,8 @@ int main(int argc, char *argv[], char *envp[])
 	memcpy(code_buf, addnum, L);
 	f = (int (*)(int)) code_buf;
 	printf("LINE2 f: %p\n", f);
+	printf("is addnum == f : %i", f == addnum);
+
 	a = f(10); // LINE2
 
 	printf("2: f(10)=%d f=%p\n\n", a, f);
