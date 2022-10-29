c9d068e097b2b1de664419f947e5eec85ce95274
Noel Lim <noellimx@gmail.com>
2022-10-29 19:20:23 +0800

    -

--- Lab7/ex2.c
+++ Lab7/ex2.c
@@ -8,8 +8,7 @@ int i;
 
 void printbytes(char *p, int j, int k) // only for convenience, not needed
 {
-  for (; j <= k; j++)
-    printf("%02d: %p : %02hhx\n", j, p + j, p[j]);
+  for (; j <= k; j++) printf("%02d: %p : %02hhx\n", j, p+j, p[j]);
 }
 
 void cookie_error() // call when error detected
@@ -18,42 +17,19 @@ void cookie_error() // call when error detected
   exit(1);
 }
 
+
 int f()
 {
-
-  char canarystart = 0;
-  char buf[8];
+  char buf[8]; 
   char c;
-  char canaryend = 0;
-  c = 'z';
-  printf("%s %c\n", buf, c);
-  printbytes(buf, 0, 9);
-  buf[8] = 'a';
-  buf[7] = 'a';
-  // buf[9] = 'a';
-  // buf[10] = 'b';
-  // printf("%s %c\n", buf, c);
-  printbytes(buf, 0, 9);
 
-  printf("%s\n", buf);
-
-  printbytes(buf, 0, 9);
-
-  printf("%c\n", c);
-
-
-
-  // LINE1 - don't change code from LINE1 to LINE2
-
-  /*
+  // LINE1 - don't change code from LINE1 to LINE2 
   printf("Enter a string: ");
-  for (i = 0; (c = getchar()) != '\n'; i++)
-    buf[i] = c;
+  for (i = 0; (c = getchar()) != '\n'; i++) buf[i] = c;
   buf[i] = '\0';
-  printf("string = [%s]\n", buf);
-  */
+  // printf("string = [%s]\n", buf);
   // LINE2
-  printf("start ... end %p %p %p %p\n", &canarystart, &buf, &c, &canaryend);
+
   return 0;
 }
 
@@ -68,9 +44,10 @@ int main(int argc, char *argv[])
   setvbuf(stdout, 0, _IONBF, 0); // use unbuffered I/O to simplify things
   setvbuf(stdin, 0, _IONBF, 0);
   setvbuf(stderr, 0, _IONBF, 0);
-  if (argc > 1 && strcmp(argv[1], "-p") == 0)
+  if (argc > 1 && strcmp(argv[1], "-p")==0)
     my_protect = 1;
-
-  printf("f = %p\n", (void *)do_not_call); // leak
+  
+  printf("f = %p\n", (void *) do_not_call); // leak
   return f();
 }
+
