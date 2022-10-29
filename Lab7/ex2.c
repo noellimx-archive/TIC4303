36693dd538c4b7dc61b682f0e260728137b6c0d3
Noel Lim <noellimx@gmail.com>
2022-10-29 18:04:20 +0800

    -

--- Lab7/ex2.c
+++ Lab7/ex2.c
@@ -8,7 +8,8 @@ int i;
 
 void printbytes(char *p, int j, int k) // only for convenience, not needed
 {
-  for (; j <= k; j++) printf("%02d: %p : %02hhx\n", j, p+j, p[j]);
+  for (; j <= k; j++)
+    printf("%02d: %p : %02hhx\n", j, p + j, p[j]);
 }
 
 void cookie_error() // call when error detected
@@ -17,23 +18,23 @@ void cookie_error() // call when error detected
   exit(1);
 }
 
-
 int f()
 {
-  char buf[8]; 
+  char buf[8];
   char c;
 
   int a;
+  char b[2];
 
-
-
-  // LINE1 - don't change code from LINE1 to LINE2 
+  // LINE1 - don't change code from LINE1 to LINE2
   printf("Enter a string: ");
-  for (i = 0; (c = getchar()) != '\n'; i++) buf[i] = c;
+  for (i = 0; (c = getchar()) != '\n'; i++)
+    buf[i] = c;
   buf[i] = '\0';
   printf("string = [%s]\n", buf);
   // LINE2
   printf("c, buf[i] %p %p %p\n", &c, buf + 8, &a);
+  printf("a, b, c %p %p %p\n", &c, &a, &b);
   return 0;
 }
 
@@ -48,10 +49,9 @@ int main(int argc, char *argv[])
   setvbuf(stdout, 0, _IONBF, 0); // use unbuffered I/O to simplify things
   setvbuf(stdin, 0, _IONBF, 0);
   setvbuf(stderr, 0, _IONBF, 0);
-  if (argc > 1 && strcmp(argv[1], "-p")==0)
+  if (argc > 1 && strcmp(argv[1], "-p") == 0)
     my_protect = 1;
-  
-  printf("f = %p\n", (void *) do_not_call); // leak
+
+  printf("f = %p\n", (void *)do_not_call); // leak
   return f();
 }
-
